1d849bc66920297178372376789bdb226aeab993
Robert Brown <brown.rjb75@gmail.com>
2021-01-16 16:56:53 -0700

    updated to parse text from a slide

--- background.js
+++ background.js
@@ -6,6 +6,8 @@ const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v
 const SPREADSHEET_ID = '1OexDEbqVQ9dvL6kTmWILcMezVtpw4ee_QfqAWghEebU';
 const SPREADSHEET_TAB_NAME = 'Sheet1';
 
+var scontent = new Array();
+
 function onGAPILoad() {
     gapi.client.init({
       // Don't pass client nor scope as these will init auth2, which we don't want
@@ -18,17 +20,27 @@ function onGAPILoad() {
           'access_token': token,
         });
   
-        // gapi.client.sheets.spreadsheets.values.get({
-        //   spreadsheetId: SPREADSHEET_ID,
-        //   range: SPREADSHEET_TAB_NAME,
-        // }).then(function(response) {
-        //   console.log(`Got ${response.result.values.length} rows back`)
-        // });
 
         gapi.client.slides.presentations.get({
             presentationId : presId,
         }).then(function(response){
-            console.log('Got slides back' + response.result.slides.length)
+
+          console.log('Presentation Title: ' + response.result.title);
+          console.log('Slides in Presentation: ' + response.result.slides.length);
+          console.log('Random Slide Text: ' + response.result.slides[0].pageElements[1].shape.text.textElements[1].textRun.content);
+          response.result.slides.forEach(slides => {
+            slides.pageElements.forEach(pageElement => {
+              if(pageElement.shape != undefined && pageElement.shape.shapeType === 'TEXT_BOX') {
+                pageElement.shape.text.textElements.forEach(element =>{
+                  if(element.textRun != undefined && element.textRun.content != " " && element.textRun.content != "") {
+                    //console.log(element.textRun.content.toString());
+                    scontent.push(element.textRun.content);
+                  }
+                });
+              }
+            });
+          });
+          console.log(scontent.length); //outputs number of text lines
         });
 
       })
