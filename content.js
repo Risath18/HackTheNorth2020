f83ae772f4cc85d86a374189e6200402f1bd76b8
Samson Hua <Samson.hua@ucalgary.ca>
2021-01-15 22:49:30 -0700

    Added Injection Test

--- content.js
+++ content.js
@@ -6,4 +6,11 @@
 //Samson Hua - The Random Mechanical Kid
 //Risat Haque - The UI God
 
-alert('Invest Today')
+<div style="padding-left: 32px; padding-right: 32px; padding-top: 29px; padding-bottom: 29px; background-color: white; display: inline-flex; flex-direction: column; align-items: flex-start; justify-content: flex-start;">
+<div style="width: 100px; height: 100px; background-color: rgba(255, 0, 0, 1);"></div>
+<div style="height: 35px;"></div>
+<div style="width: 100px; height: 100px; background-color: rgba(0, 10.20, 255, 1);"></div>
+<div style="height: 35px;"></div>
+<div style="width: 100px; height: 100px; background-color: rgba(35.70, 255, 0, 1);"></div>
+<div style="height: 35px;"></div>
+<div style="width: 100px; height: 100px; background-color: rgba(255, 0, 0, 1);"></div></div>
