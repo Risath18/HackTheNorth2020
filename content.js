3bc89fcdabb56ba75a9087581d60e37c47497d4c
Samson Hua <Samson.hua@ucalgary.ca>
2021-01-15 22:00:57 -0700

    Samson's awesome commit

---
+++ content.js
@@ -0,0 +1,9 @@
+//Created for HackTheNorth++ 2020
+
+//Creators:
+//Zachary Lau - The Algorithm God
+//Robert Brown - The Website God
+//Samson Hua - The Random Mechanical Kid
+//Risat Haque - The UI God
+
+alert('Invest Today')
