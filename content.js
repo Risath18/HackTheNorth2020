986b89b8990607b79a288f1217a1ec269b81957f
Samson Hua <Samson.hua@ucalgary.ca>
2021-01-16 09:18:47 -0700

    Tried manually adding images

--- content.js
+++ content.js
@@ -7,8 +7,14 @@
 //Risat Haque - The UI God
 
 var div = document.createElement("DIV");
-div.id = "someName";
+div.id = "scaffold";
 var img = document.createElement("IMG");
 img.src = "https://i.imgur.com/cc1Dm2s.png";
+var div2 = document.createElement("DIV");
+div2.id = "button";
+var img2 = document.createElement("IMG");
+img2.src = "https://i.imgur.com/wgzcUv6.png";
 div.appendChild(img);
+div.appendChild(img2);
 document.body.appendChild(div);
+document.body.appendChild(div2);
