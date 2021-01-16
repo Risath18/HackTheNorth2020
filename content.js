8b029efab2b9e3ba94504cccc918a66344b1f8f6
Samson Hua <Samson.hua@ucalgary.ca>
2021-01-16 09:43:11 -0700

    CSS is hard and I am bad at it

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
 //Created for HackTheNorth++ 2020
 
 //Creators:
-//Zachary Lau - The Algorithm God
-//Robert Brown - The Website God
+//Zachary Lau - The Algorithm Person
+//Robert Brown - The Website Person
 //Samson Hua - The Random Mechanical Kid
-//Risat Haque - The UI God
+//Risat Haque - The UI Person
 
+//Rip DOM how does it work!?!?!
 var div = document.createElement("DIV");
 div.id = "scaffold";
 var img = document.createElement("IMG");
 img.src = "https://i.imgur.com/cc1Dm2s.png";
+div.appendChild(img);
+document.body.appendChild(div);
+
 var div2 = document.createElement("DIV");
 div2.id = "button";
 var img2 = document.createElement("IMG");
 img2.src = "https://i.imgur.com/wgzcUv6.png";
-div.appendChild(img);
-div.appendChild(img2);
-document.body.appendChild(div);
+div2.appendChild(img2);
 document.body.appendChild(div2);
