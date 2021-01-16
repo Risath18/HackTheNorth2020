c5ad3fa0cd2e9ce73e19871edc645709c90b5353
Robert Brown <brown.rjb75@gmail.com>
2021-01-16 15:01:16 -0700

    connected to slides api

--- background.js
+++ background.js
@@ -1,26 +1,38 @@
 const API_KEY = config.API_KEY;
-const DISCOVERY_DOCS = [
-  "https://slides.googleapis.com/$discovery/rest?version=v1",
-];
+const presId = '1Znkito_qgt3xTfFf-icqOB7pQd52Mtqk0ffzTwLVsG4';
+const SLIDES_DISCOVERY_DOCS = ["https://slides.googleapis.com/$discovery/rest?version=v1",];
+const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+const SPREADSHEET_ID = '1OexDEbqVQ9dvL6kTmWILcMezVtpw4ee_QfqAWghEebU';
+const SPREADSHEET_TAB_NAME = 'Sheet1';
+
 function onGAPILoad() {
-  gapi.client
-    .init({
+    gapi.client.init({
       // Don't pass client nor scope as these will init auth2, which we don't want
       apiKey: API_KEY,
-      discoveryDocs: DISCOVERY_DOCS,
-    })
-    .then(
-      function () {
-        console.log("gapi initialized");
-      },
-      function (error) {
-        console.log("error", error);
-      }
-    );
-}
+      discoveryDocs: SLIDES_DISCOVERY_DOCS,
+    }).then(function () {
+      console.log('gapi initialized')
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        gapi.auth.setToken({
+          'access_token': token,
+        });
+  
+        // gapi.client.sheets.spreadsheets.values.get({
+        //   spreadsheetId: SPREADSHEET_ID,
+        //   range: SPREADSHEET_TAB_NAME,
+        // }).then(function(response) {
+        //   console.log(`Got ${response.result.values.length} rows back`)
+        // });
 
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  let link = document.createElement("a");
-  link.href = tabs[0].url;
-  console.log(link.href);
-});
+        gapi.client.slides.presentations.get({
+            presentationId : presId,
+        }).then(function(response){
+            console.log('Got slides back' + response.result.slides.length)
+        });
+
+      })
+    }, function(error) {
+      console.log('error', error)
+    });
+  }
