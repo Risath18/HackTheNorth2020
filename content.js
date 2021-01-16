fe43b98769d332503bcbfcb890f7baefff55ed45
Robert Brown <brown.rjb75@gmail.com>
2021-01-16 01:36:28 -0700

    made some tings work

--- content.js
+++ content.js
@@ -13,29 +13,45 @@ document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
 
 var div = document.createElement( 'div' );
-var btnForm = document.createElement( 'form' );
-var btn = document.createElement( 'input' );
+var play = document.createElement( 'div');
+var person = document.createElement( 'div' )
 
 //append all elements
 document.body.appendChild( div );
-div.appendChild( btnForm );
-btnForm.appendChild( btn );
+//document.body.appendChild( person );
+
+//div.appendChild( play );
+
 //set attributes for div
-div.id = 'myDivId';
+/*
+div.id = 'outter';
 div.style.position = 'fixed';
-div.style.top = '50%';
-div.style.left = '50%';
-div.style.width = '100%';   
-div.style.height = '100%';
-div.style.backgroundColor = 'red';
+div.style.top = '70%';
+div.style.left = '90%';
+div.style.width = '5%';   
+div.style.height = '20%';
+div.style.backgroundColor = '#C4C4C4';
+div.style.borderRadius = '168.5px 168.5px 0px 0px';
+*/
 
-//set attributes for btnForm
-btnForm.action = '';
+document.body.innerHTML += '<div class="v35_21">  <div class="v35_22" id="testimg"></div>  <div class="v35_23"></div>  <div class="v35_24"></div>  <div class="v35_25"></div></div>';
+var img = document.getElementById('testimg');
+var loho = chrome.extension.getURL('logo128.png');
+img.style.backgroundImage = loho;
+console.log("test");
+/*
+person.id = 'person';
+person.style.position = 'fixed';
+person.style.left = '90%';
+person.style.width = '5%';
+person.style.height = '5%';
+person.borderRadius = '50%';
+person.style.top = '85%';
 
-//set attributes for btn
-//"btn.removeAttribute( 'style' );
-btn.type = 'button';
-btn.value = 'hello';
-btn.style.position = 'absolute';
-btn.style.top = '50%';
-btn.style.left = '50%';
+person.style.backgroundColor = '#C4C4C4';
+
+
+play.id = 'play';
+play.borderRadius = '50%';
+play.backgroundColor = '#444444';
+*/
