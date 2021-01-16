fed06706bc874ccdd99a721fd1ed703d9e923522
Robert Brown <brown.rjb75@gmail.com>
2021-01-16 11:57:47 -0700

    added api key integration

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
-chrome.identity.getAuthToken({interactive: true}, function(token) {
-    console.log('got the token', token);
-  })
+const API_KEY = config.API_KEY;
+const DISCOVERY_DOCS = ["https://slides.googleapis.com/$discovery/rest?version=v1"];
+function onGAPILoad() {
+  gapi.client.init({
+    // Don't pass client nor scope as these will init auth2, which we don't want
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+    console.log('gapi initialized')
+  }, function(error) {
+    console.log('error', error)
+  });
+}
