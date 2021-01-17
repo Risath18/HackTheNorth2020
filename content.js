161fed9f5be7e974d136fe4ea7e6e5e4bd10a963
zach-lau <zlau2000@gmail.com>
2021-01-16 17:12:24 -0700

    Delete unused helpers

--- content.js
+++ content.js
@@ -73,16 +73,4 @@ if (!('webkitSpeechRecognition' in window)) {
   recognition.onresult.lasttime = Date.now();
 }
 
-// Helper formatting functions
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-
-var first_char = /\S/;
-function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
-}
-
 recognition.start();
