6ae1ce34791cdacc04e3ff4369e33bb1395f3c70
Zachary Lau <32786812+zach-lau@users.noreply.github.com>
2021-01-16 17:13:06 -0700

    Add wpm counter (#5)
    Adds a wpm counter at the bottom of the page

--- content.js
+++ content.js
@@ -23,3 +23,54 @@ var img2 = document.createElement("IMG");
 img2.src = "https://i.imgur.com/wgzcUv6.png";
 div2.appendChild(img2);
 document.body.appendChild(div2);
+
+//WPM Information
+var wpminfo = document.createElement("DIV");
+wpminfo.id = "wpminfo"
+document.body.appendChild(wpminfo);
+
+// Implements the listener and state handler
+
+if (!('webkitSpeechRecognition' in window)) {
+  alert("Browser does not support the extension!");
+} else {
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = false; // Unless we use this
+
+  recognition.onstart = function() {
+    // Add logic here to change the UI when we are recording
+    console.log("Starting");
+  };
+
+  // Could add an onerror or an on end thing but screw that for now 
+
+
+  recognition.onresult = function(event) {
+    // Keep track of last time we calculated wpm
+    lasttime = recognition.onresult.lasttime;
+
+    // Collect the new strings
+    var newString = "";
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        newString += event.results[i][0].transcript;
+      }
+    }
+    // Calculate wpm and publish
+    if (newString != ""){
+        var currenttime = Date.now();
+        if (lasttime != NaN){
+            var words = newString.trim().split(" ").length;
+            wpm = words / (currenttime - lasttime) * 60000; //convert ms to min
+            console.log(lasttime, currenttime, words, wpm, newString);
+            wpminfo.innerHTML = "<p> Wpm = " + wpm + "</p>"
+        }
+        recognition.onresult.lasttime = currenttime;
+    }
+    
+  };
+  recognition.onresult.lasttime = Date.now();
+}
+
+recognition.start();
